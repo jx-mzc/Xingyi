d88b9be9bcf996e9bcbd7e315318acc8296784f3
jx-mzc <42129607+jx-mzc@users.noreply.github.com>
2018-12-01 21:52:33 +0800

    Activity管理器

--- app/src/main/java/com/example/administrator/xingyi/MainActivity.java
+++ app/src/main/java/com/example/administrator/xingyi/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCollector.addActivity(this);
         //myDatabaseHelper = new MyDatabaseHelper(MainActivity.this);
        // myDatabaseHelper.getWritableDatabase();
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -88,6 +89,12 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
         setupViewPager(viewPager);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
     }
 
     private void setupViewPager(ViewPager viewPager) {
