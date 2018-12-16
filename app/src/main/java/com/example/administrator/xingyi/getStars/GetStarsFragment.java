b8fbb06aef01a894317405b1af284a7c691bf0bf
jx-mzc <42129607+jx-mzc@users.noreply.github.com>
2018-12-16 21:57:15 +0800

    赚星模块

--- app/src/main/java/com/example/administrator/xingyi/getStars/GetStarsFragment.java
+++ app/src/main/java/com/example/administrator/xingyi/getStars/GetStarsFragment.java
@@ -1,5 +1,6 @@
 package com.example.administrator.xingyi.getStars;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.example.administrator.xingyi.R;
+import com.example.administrator.xingyi.getStars.clocks.ClocksActivity;
+import com.example.administrator.xingyi.getStars.stepCount.StepCountActivity;
 import com.hjq.bar.TitleBar;
 
 /**
@@ -18,11 +22,29 @@ import com.hjq.bar.TitleBar;
  */
 public class GetStarsFragment extends Fragment {
 
+    private RelativeLayout rlStepCount;
+    private RelativeLayout rlClocks;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_getstars,null);
+        rlStepCount = view.findViewById(R.id.rv_getstars_step);
+        rlClocks = view.findViewById(R.id.rv_getstars_clocks);
+        rlStepCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), StepCountActivity.class);
+                startActivity(intent);
+            }
+        });
+        rlClocks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ClocksActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
---
+++ app/src/main/java/com/example/administrator/xingyi/getStars/stepCount/StepCountActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.xingyi.getStars.stepCount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.administrator.xingyi.R;
+
+public class StepCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_count);
+    }
+}
